dddf65c5a75b60e3f98f413e88f0b53314277dbf
Stuko0 <alexander436065lol@gmail.com>
2024-11-15 19:10:15 -0400

    Create models, controllers and database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/Stuko0/PendejadaUni/internal/database"
+	"github.com/Stuko0/PendejadaUni/pkg/models"
+	"github.com/Stuko0/PendejadaUni/pkg/routes"
+)
+
+func main() {
+	database.Connect()
+	database.DB.AutoMigrate(&models.Student{})
+	database.DB.AutoMigrate(&models.Classroom{})
+	r := routes.SetupRouter()
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
